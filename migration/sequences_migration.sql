3c8e143281275c2b04dd66a1841de45dcce39380
Valeh Agayev <valeh.agayev@gmail.com>
2026-03-18 11:05:37 +0400

    Create sequences_migration.sql

---
+++ migration/sequences_migration.sql
@@ -0,0 +1,9 @@
+DO $$
+  DECLARE
+      r RECORD;
+  BEGIN
+      FOR r IN SELECT sequence_name FROM information_schema.sequences WHERE sequence_schema = 'source_schema_name'
+      LOOP
+          EXECUTE format('ALTER SEQUENCE public.%I SET SCHEMA target_schema_name', r.sequence_name);
+      END LOOP;
+  END $$;
